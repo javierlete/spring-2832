13b14767020a0de7a983abcf66d78461a690a80c
javier <javier@ipartekaula.com>
2025-03-11 20:19:52 +0100

    Copia de entidad del otro proyecto

---
+++ ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/entidades/Producto.java
@@ -0,0 +1,41 @@
+package com.ipartek.formacion.ejemplospring.entidades;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+@Entity
+@Table(name="productos")
+public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	private String nombre;
+	
+	@NotNull
+	@Min(0)
+	private BigDecimal precio;
+	
+	@FutureOrPresent
+	private LocalDate fechaCaducidad;
+}
