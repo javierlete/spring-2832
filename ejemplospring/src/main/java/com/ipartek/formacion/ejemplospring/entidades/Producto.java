7bc3c0c160a920264f734fd3244283471796a082
javier <javier@ipartekaula.com>
2025-03-11 20:31:14 +0100

    Mejoras de producto

--- ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/entidades/Producto.java
+++ ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/entidades/Producto.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,19 +25,24 @@ import lombok.NoArgsConstructor;
 @Data
 
 @Entity
-@Table(name="productos")
+@Table(name = "productos")
 public class Producto {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank
+	@Size(max = 100)
 	private String nombre;
-	
+
 	@NotNull
 	@Min(0)
 	private BigDecimal precio;
-	
+
 	@FutureOrPresent
 	private LocalDate fechaCaducidad;
+
+	@Lob
+	@Size(max = 2000)
+	private String descripcion;
 }
