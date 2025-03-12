a1d0c7f9a3eccbfceea3a6a26efcda9f575ff9d9
javier <javier@ipartekaula.com>
2025-03-12 20:55:50 +0100

    Funcionalidad completa de guardar

--- ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/AdminServiceImpl.java
+++ ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/AdminServiceImpl.java
@@ -1,18 +1,23 @@
 package com.ipartek.formacion.ejemplospring.servicios;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
+import com.ipartek.formacion.ejemplospring.repositorios.ProductoRepository;
 
 import lombok.extern.java.Log;
 
 @Log
 @Service
 public class AdminServiceImpl implements AdminService {
-
+	@Autowired
+	private ProductoRepository repo;
+	
 	@Override
 	public void crearProducto(Producto producto) {
 		log.info("Guardando producto " + producto);
+		repo.save(producto);
 	}
 
 }
