d20b0e75c9c17f031ba05b59dfe9f4f3296ea1a1
javier <javier@ipartekaula.com>
2025-03-12 20:08:27 +0100

    El controlador delega la visualización de principal en thymeleaf

--- ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/controladores/AnonimoController.java
+++ ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/controladores/AnonimoController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ipartek.formacion.ejemplospring.servicios.AnonimoService;
 
@@ -15,10 +14,9 @@ public class AnonimoController {
 	private AnonimoService servicio;
 	
 	@GetMapping("/")
-	@ResponseBody
 	public String principal(Model modelo) {
 		var productos = servicio.obtenerProductos(); 
 		modelo.addAttribute("productos", productos);
-		return modelo.toString();
+		return "principal";
 	}
 }
