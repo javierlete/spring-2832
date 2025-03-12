2f16870a94c397565025ae22c0f5be4a72ce5e99
javier <javier@ipartekaula.com>
2025-03-12 19:08:13 +0100

    Hacemos una implementación de las operaciones de anónimos con @Log

---
+++ ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/servicios/AnonimoServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ipartek.formacion.ejemplospring.servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ipartek.formacion.ejemplospring.entidades.Producto;
+import com.ipartek.formacion.ejemplospring.repositorios.ProductoRepository;
+
+import lombok.extern.java.Log;
+
+@Log
+@Service
+public class AnonimoServiceImpl implements AnonimoService {
+	
+	@Autowired
+	private ProductoRepository repo;
+	
+	@Override
+	public Iterable<Producto> obtenerProductos() {
+		log.info("Anónimo ha pedido todos los productos");
+		return repo.findAll();
+	}
+
+	@Override
+	public Producto detalleProducto(Long id) {
+		log.info("Anónimo ha pedido el producto id " + id);
+		return repo.findById(id).orElse(null);
+	}
+	
+}
