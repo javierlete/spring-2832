a8d8940d72caebf997464ac9d753f26875e1137a
javier <javier@ipartekaula.com>
2025-03-13 20:42:55 +0100

    Quitamos el guardado de ejemplo de consola

--- ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/EjemplospringApplication.java
+++ ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/EjemplospringApplication.java
@@ -1,13 +1,10 @@
 package com.ipartek.formacion.ejemplospring;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.ipartek.formacion.ejemplospring.entidades.Producto;
 import com.ipartek.formacion.ejemplospring.repositorios.ProductoRepository;
 
 @SpringBootApplication
@@ -22,7 +19,7 @@ public class EjemplospringApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repo.save(Producto.builder().nombre("Prueba").precio(new BigDecimal("1234")).build());
+//		repo.save(Producto.builder().nombre("Prueba").precio(new BigDecimal("1234")).build());
 		
 		for(var p: repo.findAll()) {
 			System.out.println(p);
