bfc45bc581adc814b164bb0e79a97c640ea71bdd
javier <javier@ipartekaula.com>
2025-03-13 19:23:42 +0100

    Formulario con validaciones y mensajes originales

--- ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/controladores/AdminController.java
+++ ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/controladores/AdminController.java
@@ -2,12 +2,15 @@ package com.ipartek.formacion.ejemplospring.controladores;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ipartek.formacion.ejemplospring.entidades.Producto;
 import com.ipartek.formacion.ejemplospring.servicios.AdminService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/admin")
@@ -16,12 +19,16 @@ public class AdminController {
 	private AdminService servicio;
 
 	@GetMapping("/producto")
-	public String verFormularioProducto() {
+	public String verFormularioProducto(Producto producto) {
 		return "producto";
 	}
 	
 	@PostMapping("/producto")
-	public String recibirFormularioProducto(Producto producto) {
+	public String recibirFormularioProducto(@Valid Producto producto, BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {
+			return "producto";
+		}
+		
 		servicio.crearProducto(producto);
 		return "redirect:/";
 	}
