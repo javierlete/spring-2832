60d7b43975600fe16bca69b7f16d626f8540b418
javier <javier@ipartekaula.com>
2025-03-18 19:27:47 +0100

    Autenticación "hardcodeada" y autorizaciones

---
+++ ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/configuraciones/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package com.ipartek.formacion.ejemplospring.configuraciones;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+	// AUTENTICACIÓN
+	@Bean
+	UserDetailsService userDetailsService() {
+		UserDetails javier =
+			 User.withDefaultPasswordEncoder()
+				.username("javier")
+				.password("admin")
+				.roles("ADMIN")
+				.build();
+		
+		UserDetails pepe =
+			 User.withDefaultPasswordEncoder()
+				.username("pepe")
+				.password("contra")
+				.roles("USER")
+				.build();
+			
+		return new InMemoryUserDetailsManager(javier, pepe);
+	}
+	
+	// AUTORIZACIÓN
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests((requests) -> requests
+				.requestMatchers("/admin/**").hasRole("ADMIN")
+				.requestMatchers("/detalle").authenticated()
+				.anyRequest().permitAll()
+			)
+			.formLogin((form) -> form
+			//	.loginPage("/login")
+				.permitAll()
+			)
+			// .logout((logout) -> logout.permitAll())
+			;
+
+		return http.build();
+	}
+}
