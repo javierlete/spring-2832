1fb89047bccf6f224bf09a867df1e2a32f69f32c
javier <javier@ipartekaula.com>
2025-03-18 20:28:18 +0100

    Seguridad a través de JDBC con tablas por defecto

--- ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/configuraciones/WebSecurityConfig.java
+++ ejemplospring/src/main/java/com/ipartek/formacion/ejemplospring/configuraciones/WebSecurityConfig.java
@@ -1,36 +1,35 @@
 package com.ipartek.formacion.ejemplospring.configuraciones;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
 	// AUTENTICACIÓN
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+	    auth.jdbcAuthentication().dataSource(dataSource);
+	}
+
 	@Bean
-	UserDetailsService userDetailsService() {
-		UserDetails javier =
-			 User.withDefaultPasswordEncoder()
-				.username("javier")
-				.password("admin")
-				.roles("ADMIN")
-				.build();
-		
-		UserDetails pepe =
-			 User.withDefaultPasswordEncoder()
-				.username("pepe")
-				.password("contra")
-				.roles("USER")
-				.build();
-			
-		return new InMemoryUserDetailsManager(javier, pepe);
+	PasswordEncoder passwordEncoder() {
+	    // return new BCryptPasswordEncoder();
+		return NoOpPasswordEncoder.getInstance();
 	}
 	
 	// AUTORIZACIÓN
